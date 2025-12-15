b403c5ce6366ac3d0fae48a23f47c1d553b94a12
Johannes Rosenberger <johannes@jorsn.eu>
2025-12-15 21:23:32 +0100

    add example (ragenix --rekey)

---
+++ example_agenix_rekey/secrets.nix
@@ -0,0 +1,9 @@
+{
+  "secret.txt.age".publicKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsmOZmlB/ORKJ3Z03XNbZcUjIyXtLaIO8HjIOa068Uf johannes@longitude-j"
+
+  ];
+  "secret2.txt.age".publicKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsmOZmlB/ORKJ3Z03XNbZcUjIyXtLaIO8HjIOa068Uf johannes@longitude-j"
+  ];
+}
